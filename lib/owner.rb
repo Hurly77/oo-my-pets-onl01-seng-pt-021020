44c971e3207394c04630672018cb0efcbde17827
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-03-09 21:10:07 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
+require 'pry'
 class Owner
-  # code goes here
+attr_accessor
+attr_reader :name, :species
+@@all = []
+def initialize(name)
+  @name = name
+  @species = "human"
+  @@all << self
+
+end
+
+def say_species
+  says ="I am a human."
+  says
+end
+
+def self.all
+@@all
+end
+
+def self.count
+the_count = self.all.count {|owner| owner == Owner.new(name }
+the_count
+binding.pry
+end
+
 end
