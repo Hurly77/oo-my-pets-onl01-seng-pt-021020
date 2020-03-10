d3901f4ea6694915a1c9f4a32c01c67ebf8c78be
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-03-10 03:31:21 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,14 @@
 require 'pry'
 class Owner
-attr_accessor
+attr_accessor :cat
 attr_reader :name, :species
 @@all = []
 def initialize(name)
   @name = name
   @species = "human"
   @@all << self
+  @cats = []
+
 
 end
 
@@ -20,9 +22,45 @@ def self.all
 end
 
 def self.count
-the_count = self.all.count {|owner| owner == Owner.new(name }
-the_count
-binding.pry
+self.all.count
 end
 
+def self.reset_all
+@@all.clear
+end
+
+def cats
+Cat.all.select {|cat| cat.owner == self}
+
+end
+
+def dogs
+  Dog.all.select {|dog| dog.owner == self}
+end
+
+def buy_cat(name)
+  name = Cat.new(name, self)
+end
+
+def buy_dog(name)
+  name = Dog.new(name, self)
+end
+
+def walk_dogs
+dogs.map {|dog| dog.mood = "happy"}
+end
+
+def feed_cats
+  cats.map {|cat| cat.mood = "happy"}
+end
+
+def sell_pets
+  dogs.map {|dog| dog.mood = "nervous"}
+  cats.map {|cat| cat.mood = "nervous"}
+  dogs.select {|dog| dog.owner = nil}
+  cats.select {|cat| cat.owner = nil}
+end
+def list_pets
+return "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+end
 end
